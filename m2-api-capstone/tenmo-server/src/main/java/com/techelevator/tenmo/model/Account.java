2d83317e9dfd94f39fe7dfa4eab1ad4a54117c79
Mohammed Fofanah <mohammedfofanah@gmail.com>
2021-11-03 13:58:13 -0400

    objects and Dao set up

---
+++ m2-api-capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,43 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private int accountId;
+    private int userId;
+    private BigDecimal balance;
+    private String userName;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
